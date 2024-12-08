d73982caec36d50324237ec036caba47ae178de3
Johnson <sakajohnson1@gmail.com>
2024-12-08 19:03:18 +0000

    first commit

---
+++ login.js
@@ -0,0 +1,56 @@
+
+// valid credentials
+
+const validusername = "Walker";
+
+const validpassword = "saka1nero2";
+
+// getting form elements
+
+
+document.getElementById("login-button").addEventListener("click",(e)=>{
+
+  const username = document.getElementById("Username").value;
+  const password = document.getElementById("Password").value;
+
+  if (username === validusername && password === validpassword){
+    document.getElementById("message").innerHTML ="Login successful!"
+    document.getElementById("message").style.color = "white";
+
+    setTimeout( () => {
+      window.location.href = 'success.html';
+    } , 2000)
+  }else{
+    document.getElementById("message").innerHTML="Login failed!"
+    document.getElementById("message").style.color = "red"
+  }
+
+
+});
+
+
+// event listener for form submission
+
+/*form.addEventListener("submit" ,(event)=>{
+
+  // getting user input
+  const username = document.getElementById("Username").value;
+
+  const password = document.getElementById("Password").value;
+
+  // check credentials
+
+  if (username === validusername && password === validpassword){
+    messageBox.style.color = "green";
+    messageBox.textContent = "Login successful";
+    alert("Login successful");
+
+    // set local storage to indicate user is logged in
+    localStorage.setItem("isLoggedIn", true);
+
+  }else{
+    messageBox.style.color ="red";
+    messageBox.textContent = "Invalid username or password";
+  }
+});
+*/
